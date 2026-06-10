99b28dad41c92b678fac265be166f37d9ba2027f
Your Name <you@example.com>
2026-06-10 20:46:01 +0000

    gale: restore HIDL bluetooth support

--- device.mk
+++ device.mk
@@ -74,8 +74,10 @@ $(call soong_config_set_bool,android_hardware_audio,skip_speaker_layout_channel_
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth-service.mediatek
-
+    android.hardware.bluetooth.audio-impl:32 \
+    android.hardware.bluetooth@1.1.vendor:32 \
+    libbluetooth_audio_session:32
+    
 # Cgroup
 PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
